78dcb321ade3bb08e493e6b8656e3070e60217f6
aeysaman <aldojane@gmail.com>
2016-04-01 19:55:15 -0400

    built export functionality

--- Program.cs
+++ Program.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
 using System.Net;
 using System.IO;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Web;
@@ -19,38 +16,32 @@ namespace LiveChat_Gathering
         static void Main(string[] args)
         {
             LiveChatApi.Api Api = new LiveChatApi.Api("a.tomic@bc.edu", "3302ee3a4cfd5ba8cadb2515df64168e");
-            //getAgent(Api).Wait();
-            getChat(Api).Wait();
-            //getTotalChats(Api).Wait();
+            getChats(Api).Wait();
         }
-        static async Task getAgents(LiveChatApi.Api Api)
+        static void exportChats(ChatGroup chats)
         {
-            string result = await Api.Agents.List();
-            Console.WriteLine(result);
-            Console.ReadKey();
-        }
-        static async Task getTotalChats(LiveChatApi.Api Api)
-        {
-            Dictionary<string, string> parameters = new Dictionary<string, string>();
-            parameters.Add("date_from", "2015-10-01");
-            parameters.Add("date_to", "2016-01-31");
-            parameters.Add("agent", "a.tomic@bc.edu");
-            parameters.Add("group_by", "hour");
-            string result = await Api.Reports.ChatSources(parameters);
-
-            Console.WriteLine(result);
+            Console.WriteLine("export" + chats.chats.Length);
+            var csv = new StringBuilder();
+            foreach(Chat c in chats.chats)
+            {
+                String s = exportChat(c);
+                Console.WriteLine(s);
+                csv.AppendLine(s);
+            }
+            File.WriteAllText("chats.csv", csv.ToString());
             Console.ReadKey();
         }
         static async Task getChats(LiveChatApi.Api Api)
         {
             Dictionary<string, string> parameters = new Dictionary<string, string>();
             parameters.Add("date_from", "2015-03-01");
-            parameters.Add("query", "test");
             parameters.Add("agent", "a.tomic@bc.edu");
             string result = await Api.Archives.Get(parameters);
-
-            Console.WriteLine(result);
-            Console.ReadKey();
+            DataContractJsonSerializer json = new DataContractJsonSerializer(typeof(ChatGroup));
+            MemoryStream ms = new MemoryStream(System.Text.ASCIIEncoding.ASCII.GetBytes(result));
+            ChatGroup chats = (ChatGroup)json.ReadObject(ms);
+            Console.WriteLine("getChats" + chats.chats.Length);
+            exportChats(chats);
         }
         static async Task getChat(LiveChatApi.Api Api)
         {
@@ -62,60 +53,44 @@ namespace LiveChat_Gathering
             Chat c = (Chat)json.ReadObject(ms);
             Console.WriteLine(c.type + " " + c.visitor_ip);
 
-            //Console.WriteLine(result);
             Console.ReadKey();
         }
-        static async Task getAgent(LiveChatApi.Api Api)
+        [DataContract]
+        public class ChatGroup
         {
-            Dictionary<string, string> parameters = new Dictionary<string, string>();
-            parameters.Add("login", "a.tomic@bc.edu");
-            string result = await Api.Agents.Add(parameters);
-            DataContractJsonSerializer test = new DataContractJsonSerializer(typeof(Agent));
-
-            Console.WriteLine(result);
-            Console.ReadKey();
+            [DataMember]
+            public Chat[] chats;
+            [DataMember]
+            public int total;
+            [DataMember]
+            public int pages;
+        }
+        public static String exportChat(Chat c)
+        {
+            return c.id +"," + c.visitor + "," + c.duration + "," + c.started + "," + c.ended + "," + c.messages.Length;
         }
         [DataContract]
         public class Chat
         {
-            [DataMember]
-            public string type;
-            [DataMember]
-            public string id;
-            [DataMember]
-            public string[] tickets;
-            [DataMember]
-            public string visitor_name;
-            [DataMember]
-            public string visitor_ip;
-            [DataMember]
-            public string message_id;
-            [DataMember]
-            public Visitor visitor;
-            [DataMember]
-            public Message[] messages;
-            [DataMember]
-            public Agent[] agents;
-            [DataMember]
-            public string rate;
-            [DataMember]
-            public int duration;
-            [DataMember]
-            public string chat_start_url;
-            [DataMember]
-            public string referrer;
-            [DataMember]
-            public bool pending;
-            [DataMember]
-            public string timezone;
-            [DataMember]
-            public string started;
-            [DataMember]
-            public string started_timestamp;
-            [DataMember]
-            public string ended_timestamp;
-            [DataMember]
-            public string ended;
+            [DataMember] public string type;
+            [DataMember] public string id;
+            [DataMember] public string[] tickets;
+            [DataMember] public string visitor_name;
+            [DataMember] public string visitor_ip;
+            [DataMember] public string message_id;
+            [DataMember] public Visitor visitor;
+            [DataMember] public Message[] messages;
+            [DataMember] public Agent[] agents;
+            [DataMember] public string rate;
+            [DataMember] public int duration;
+            [DataMember] public string chat_start_url;
+            [DataMember] public string referrer;
+            [DataMember] public bool pending;
+            [DataMember] public string timezone;
+            [DataMember] public string started;
+            [DataMember] public string started_timestamp;
+            [DataMember] public string ended_timestamp;
+            [DataMember] public string ended;
         }
         [DataContract]
         public class Message
